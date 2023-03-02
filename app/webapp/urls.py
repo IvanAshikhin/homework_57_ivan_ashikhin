cf85895ab55d84099e63c00a7149e932f108ce02
Ivan <ashikhin1998@gmail.com>
2023-03-02 22:12:37 +0600

    Детальный просмотр
    Реализовал страницу детального просмотра задачи

--- app/webapp/urls.py
+++ app/webapp/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
 from webapp.views.base import IndexView
+from webapp.views.tasks import TaskDetail
 
 urlpatterns = [
     path("", IndexView.as_view(), name="index_page"),
-    path("article/", IndexView.as_view()),
+    path("task/", IndexView.as_view()),
+    path('task/<int:pk>', TaskDetail.as_view(), name='detail_task'),
 
 ]
