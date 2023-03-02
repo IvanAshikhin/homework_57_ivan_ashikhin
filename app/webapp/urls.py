8fadaa27bd0c5209e1fb1ed3dce45aad5c27191f
Ivan <ashikhin1998@gmail.com>
2023-03-02 22:55:38 +0600

    Редактирование
    Реализовал редактирование страницы

--- app/webapp/urls.py
+++ app/webapp/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 
 from webapp.views.base import IndexView
 from webapp.views.tasks import TaskDetail
+from webapp.views.tasks import TaskUpdateView
 
 urlpatterns = [
     path("", IndexView.as_view(), name="index_page"),
     path("task/", IndexView.as_view()),
     path('task/<int:pk>', TaskDetail.as_view(), name='detail_task'),
+    path('task/<int:pk>/update/', TaskUpdateView.as_view(), name='task_update')
 
 ]
--- app/webapp/views/tasks.py
+++ app/webapp/views/tasks.py
@@ -1,7 +1,11 @@
-from django.shortcuts import get_object_or_404
-from django.views.generic import TemplateView
+from datetime import datetime
+
+from django.shortcuts import get_object_or_404, redirect, render
+from django.views.generic import TemplateView, RedirectView
 
 from webapp.models import Task
+
+from webapp.forms import TaskForm
 
 
 class TaskDetail(TemplateView):
@@ -11,3 +15,37 @@ class TaskDetail(TemplateView):
         context = super().get_context_data(**kwargs)
         context['tasks'] = get_object_or_404(Task, pk=kwargs['pk'])
         return context
+
+
+class IndexRedirectView(RedirectView):
+    pattern_name = 'index_page'
+
+class TaskUpdateView(TemplateView):
+    template_name = 'task_update.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tasks'] = get_object_or_404(Task, pk=kwargs['pk'])
+        context['form'] = TaskForm(instance=context['tasks'])
+        return context
+
+    def post(self, request, *args, **kwargs):
+        tasks = get_object_or_404(Task, pk=kwargs['pk'])
+        form = TaskForm(request.POST, instance=tasks)
+        if form.is_valid():
+            tasks.edit_time = datetime.now()
+            tasks.save()
+            form.save()
+            return redirect('detail_task', pk=tasks.pk)
+        return render(request, 'task_update.html', context={'form': form, 'tasks': tasks})
+
+
+class TaskDelete(TemplateView):
+    template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        tasks = get_object_or_404(Task, pk=kwargs['pk'])
+        tasks.type.clear()
+        tasks.delete()
+
+
